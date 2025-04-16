42344e621048b1ae7a492b2c0f9fe7a2c954a855
Chubin Zhang <1175085991@bupt.edu.cn>
2025-04-16 22:15:38 +0800

    Refactor MainView to boundary package according to MVC pattern

---
+++ FinanceTracker/src/main/java/com/financetracker/boundary/MainView.java
@@ -0,0 +1,46 @@
+package com.financetracker.boundary;
+
+import com.financetracker.boundary.analysis.AnalysisView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * The main view of the application containing the tab navigation.
+ * This is a boundary class in the MVC pattern.
+ */
+public class MainView extends BorderPane {
+
+    private TabPane tabPane;
+
+    public MainView() {
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        tabPane = new TabPane();
+
+        // Create tabs for different sections
+        Tab dashboardTab = new Tab("Dashboard");
+        dashboardTab.setClosable(false);
+        // Will be implemented later
+
+        Tab transactionTab = new Tab("Transactions");
+        transactionTab.setClosable(false);
+        // Will be implemented later
+
+        Tab budgetTab = new Tab("Budget");
+        budgetTab.setClosable(false);
+        // Will be implemented later
+
+        Tab analysisTab = new Tab("Analysis");
+        analysisTab.setClosable(false);
+        analysisTab.setContent(new AnalysisView());
+
+        // Add tabs to the tab pane
+        tabPane.getTabs().addAll(dashboardTab, transactionTab, budgetTab, analysisTab);
+
+        // Set the tab pane as the center content
+        setCenter(tabPane);
+    }
+}
