8a67ca9dbbecaed5f3b2ace66f6b54538f5b67ca
Chubin Zhang <1175085991@bupt.edu.cn>
2025-04-16 21:25:40 +0800

    Add main application class with JavaFX setup

---
+++ FinanceTracker/src/main/java/com/financetracker/FinanceTrackerApp.java
@@ -0,0 +1,26 @@
+package com.financetracker;
+
+import com.financetracker.ui.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The main class for the Finance Tracker application.
+ */
+public class FinanceTrackerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainView mainView = new MainView();
+        Scene scene = new Scene(mainView, 1000, 700);
+        
+        primaryStage.setTitle("Finance Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+} 
